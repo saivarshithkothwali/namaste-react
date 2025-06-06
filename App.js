fe3bca7c7018fd16af06f26b6ca4ec2a2d2d95e9
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-06-06 14:26:44 +0530

    Nested structure with multiple children

--- App.js
+++ App.js
@@ -4,11 +4,17 @@
 const parent=React.createElement(
   "div",
   {id:"parent"},
-  React.createElement(
+  [
+    React.createElement(
     "div",
     {id:"child"},
-    [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
-  )
+    [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]),
+    React.createElement(
+      "div",
+      {id:"child"},
+      [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")])
+    ]
+  
 );
 
 console.log(parent);
