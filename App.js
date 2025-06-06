b6bb36715aacd1810ad2b850f80281ad5b052242
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-06-06 13:36:56 +0530

    Nested structure with multiple children

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
-const heading=React.createElement("h1",{id:"heading"},"Hello world from React");
-console.log(heading); //This returns us with an JS object also called as react element object
+// const heading=React.createElement("h1",{id:"heading"},"Hello world from React");
+// console.log(heading); //This returns us with an JS object also called as react element object
+
+const parent=React.createElement(
+  "div",
+  {id:"parent"},
+  React.createElement(
+    "div",
+    {id:"child"},
+    [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
+  )
+);
+
+console.log(parent);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
