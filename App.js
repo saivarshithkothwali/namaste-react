b7f2731f759f5b2c1f097715c5c21a3c1ec711f3
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-06-07 16:06:10 +0530

    Nested structure with multiple children minor changes

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
 // const heading=React.createElement("h1",{id:"heading"},"Hello world from React");
 // console.log(heading); //This returns us with an JS object also called as react element object
 
-const parent=React.createElement(
-  "div",
-  {id:"parent"},
-  [
-    React.createElement(
-    "div",
-    {id:"child"},
-    [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]),
-    React.createElement(
-      "div",
-      {id:"child"},
-      [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")])
+const parent=React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child"},[
+      React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")
+    ]),
+    React.createElement("div",{id:"child"},[
+      React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")
+    ])
     ]
   
 );
@@ -20,5 +15,7 @@ const parent=React.createElement(
 console.log(parent);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(root);
 
 root.render(parent);
