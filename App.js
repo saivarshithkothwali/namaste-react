e839c091092853f4d446c8095c0424d1ffeb071b
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-06-10 13:35:43 +0530

    importing parcel and importing react,reactDOM without CDN links

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 // const heading=React.createElement("h1",{id:"heading"},"Hello world from React");
 // console.log(heading); //This returns us with an JS object also called as react element object
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent=React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child"},[
-      React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")
+      React.createElement("h1",{},"This is namaste React"),React.createElement("h2",{},"I am h2 tag")
     ]),
     React.createElement("div",{id:"child"},[
       React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")
