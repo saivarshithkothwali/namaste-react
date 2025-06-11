47519ad71f0f2b4342445d807aa274332a974a33
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-06-11 14:21:00 +0530

    Using jsx

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
-// const heading=React.createElement("h1",{id:"heading"},"Hello world from React");
-// console.log(heading); //This returns us with an JS object also called as react element object
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent=React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child"},[
-      React.createElement("h1",{},"This is namaste React"),React.createElement("h2",{},"I am h2 tag")
-    ]),
-    React.createElement("div",{id:"child"},[
-      React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")
-    ])
-    ]
-  
-);
+const heading=React.createElement("h1",{id:"heading"},"Namaste React");
+console.log(heading);
 
-console.log(parent);
+const jsxHeading=<h1>Namaste React using jsx</h1>
+console.log(jsxHeading);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-console.log(root);
+root.render(jsxHeading);
 
-root.render(parent);
+
+
