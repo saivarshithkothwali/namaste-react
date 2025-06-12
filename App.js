d920b4f0dcc1f9d57fcfda32388986928786badd
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-06-12 19:25:42 +0530

    React functional components

--- App.js
+++ App.js
@@ -2,15 +2,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading=React.createElement("h1",{id:"heading"},"Namaste React");
-console.log(heading);
-
-const jsxHeading=<h1>Namaste React using jsx</h1>
+const jsxHeading=<h1 id="heading" tabIndex={1}>Namaste React using jsx</h1>
 console.log(jsxHeading);
+
+
+const HeadingComponent=()=>(<h1 className="heading">
+  Namaste React functional component
+  </h1>
+  );
+
+const HeadingComponent1=()=>{
+  return <h1 className="heading">Namaste React functional component1</h1>
+};
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent1/>);
+
 
 
 
