846d390f36437463683893565f3cb3b3995fd82c
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-06-12 23:17:40 +0530

    Using normal JS function for React Functional Components

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading=<h1 id="heading" tabIndex={1}>Namaste React using jsx</h1>
-console.log(jsxHeading);
+const Title=()=>(
+  <h1 id="heading" tabIndex={1}>Namaste React using jsx</h1>
+);
 
-
-const HeadingComponent=()=>(<h1 className="heading">
-  Namaste React functional component
-  </h1>
-  );
-
-const HeadingComponent1=()=>{
-  return <h1 className="heading">Namaste React functional component1</h1>
+const HeadingComponent=function(){
+    return(<div id="container">
+       <Title/>
+     <h1 className="heading">Namaste React functional component</h1>
+    </div>)
 };
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent1/>);
+root.render(<HeadingComponent/>);
 
 
 
