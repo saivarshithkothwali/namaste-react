666b7956efa5bc145db4aaa9e5cad1f52f65c89c
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-06-12 23:18:49 +0530

    Using arrow functions for react Functional components

--- App.js
+++ App.js
@@ -5,12 +5,12 @@ const Title=()=>(
   <h1 id="heading" tabIndex={1}>Namaste React using jsx</h1>
 );
 
-const HeadingComponent=function(){
-    return(<div id="container">
+const HeadingComponent=()=>(
+    <div id="container">
        <Title/>
      <h1 className="heading">Namaste React functional component</h1>
-    </div>)
-};
+    </div>
+);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
