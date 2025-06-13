6a1524230f3a9ff77397af1aef54bc09a165b3ba
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-06-13 11:13:55 +0530

    Using javascript code inside jsx

--- App.js
+++ App.js
@@ -4,10 +4,13 @@ import ReactDOM from "react-dom/client";
 const Title=()=>(
   <h1 id="heading" tabIndex={1}>Namaste React using jsx</h1>
 );
-
+const num=100;
 const HeadingComponent=()=>(
     <div id="container">
-       <Title/>
+      {"javscript inside a functional component"}
+      {10000}
+      {console.log(num)}
+      {num}
      <h1 className="heading">Namaste React functional component</h1>
     </div>
 );
