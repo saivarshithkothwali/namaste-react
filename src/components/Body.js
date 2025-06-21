a3be5fb742cdc5e9b84e1a704f580fb64beafcf1
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-06-21 23:56:26 +0530

    Rendering the restraunt data from swiggy live API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,27 @@
 import RestrauntCard from "./RestrauntCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export const Body = () => {
-  const [listOfRestraunts, setListOfRestraunt] = useState(resList);
+const Body = () => {
+  const [listOfRestraunts, setListOfRestraunts] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.002668480081386&lng=79.54484011977911&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+
+    setListOfRestraunts(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+
+  console.log("Body Rendered");
 
   return (
     <div className="body">
@@ -14,10 +32,22 @@ export const Body = () => {
             const filteredList = listOfRestraunts.filter(
               (res) => res.info.avgRating > 4.0
             );
-            setListOfRestraunt(filteredList);
+            setListOfRestraunts(filteredList);
           }}
         >
           Ratings 4.0+
+        </button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestraunts.filter((res) => {
+              const cost = parseInt(res.info.costForTwo.match(/\d+/)[0]);
+              return cost <= 300;
+            });
+            setListOfRestraunts(filteredList);
+          }}
+        >
+          Less than ₹300
         </button>
       </div>
       <div className="res-container">
