2b8cf8d410ba5d7bc26a22d613c888b037245152
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-07-02 16:21:04 +0530

    Creating Class , Functional Components and passing props while Redering

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,18 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
   return (
     <div>
       <h1>About</h1>
+      <User
+        name={"varshith(functional Component)"}
+        location={"Hanamkonda(functional Component)"}
+      />
+      <UserClass
+        name={"varshith(Class Component)"}
+        ocation={"Hanamkonda(Class Component)"}
+      />
     </div>
   );
 };
---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+const User = (props) => {
+  return (
+    <div className="user-card">
+      <h3>Name: {props.name}</h3>
+      <h3>Location: {props.location}</h3>
+    </div>
+  );
+};
+
+export default User;
