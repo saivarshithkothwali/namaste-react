333126d3fee64b747fa07825e6c42bf3d05a1629
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-07-03 22:55:03 +0530

    State Variable in Class based component and Functional Component

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
+
 const User = (props) => {
+  const [state, setState] = useState(0);
+  const [state1, setState1] = useState(1);
   return (
     <div className="user-card">
+      <h3>{state}</h3>
+      <button onClick={() => setState(state + 1)}>Increment State</button>
+      <h3>{state1}</h3>
       <h3>Name: {props.name}</h3>
       <h3>Location: {props.location}</h3>
     </div>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,11 +4,18 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     console.log(props);
+
+    this.state = {
+      count: 0,
+      count1: 1,
+    };
   }
 
   render() {
     return (
       <div className="user-card">
+        <h3>{this.state.count}</h3>
+        <h3>{this.state.count1}</h3>
         <h3>Name: {this.props.name}</h3>
         <h3>Location: {this.props.location}</h3>
       </div>
