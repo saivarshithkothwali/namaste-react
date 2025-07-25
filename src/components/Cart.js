002e2a90956050f2902c12a5e4a5308bc9879d37
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-07-26 00:42:13 +0530

    Made changes to the cart route for better UI

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -14,15 +14,25 @@ const Cart = () => {
   return (
     <div className="text-center p-4 m-4">
       <h1 className="text-2xl text-center  font-bold"> cart </h1>
-      <div className="w-8/12 mx-auto flex justify-end">
-        <button
-          className="px-[14px] py-[6px] text-sm text-red-600 border border-red-600 rounded-md cursor-pointer transition-all duration-200 ease-in-out hover:bg-red-600 hover:text-white hover:scale-105 hover:shadow-md"
-          onClick={handleClearCart}
-        >
-          Clear Cart
-        </button>
-      </div>
+      {cartItems.length !== 0 && (
+        <div className="w-8/12 mx-auto flex justify-end">
+          <button
+            className="px-[14px] py-[6px] text-sm text-red-600 border border-red-600 rounded-md cursor-pointer transition-all duration-200 ease-in-out hover:bg-red-600 hover:text-white hover:scale-105 hover:shadow-md mt-4"
+            onClick={handleClearCart}
+          >
+            Clear Cart
+          </button>
+        </div>
+      )}
       <div className="w-8/12 mx-auto my-6 bg-gray-100 drop-shadow-xl p-2  ">
+        {cartItems.length === 0 && (
+          <div className="text-center text-gray-800 m-auto">
+            <h1 className="text-xl font-semibold">Your cart is empty</h1>
+            <h3 className="text-md text-gray-600">
+              You can go to home page to view more restaurants
+            </h3>
+          </div>
+        )}
         <ItemList items={cartItems} />
       </div>
     </div>
