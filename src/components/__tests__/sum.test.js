59efd062862f44410c829a2a5d909725ac938984
Sai Varshith Kothwali <kothwalisaivarshith@gmail.com>
2025-10-13 22:21:41 +0530

    failed test cases

--- src/components/__tests__/sum.test.js
+++ src/components/__tests__/sum.test.js
@@ -2,6 +2,7 @@ import { sum } from "../sum";
 
 test("sum function should calculate the sum of two functions", () => {
   const result = sum(2, 3);
+  console.log(typeof result);
   //Assertion
   expect(result).toBe(5);
 });
